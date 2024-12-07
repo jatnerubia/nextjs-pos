919d87e905a8962490fb51c12d9ca860008d71d0
jatnerubia <jasonlerit077@gmail.com>
2024-12-07 22:51:03 +0800

    feat: increase and decrease order

--- src/components/shared/cart.tsx
+++ src/components/shared/cart.tsx
@@ -1,19 +1,46 @@
+import { Button } from "@/components/ui/button"
 import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { useOrderStore } from "@/stores/use-order.store"
+import { ChevronDownIcon, ChevronUpIcon } from "@radix-ui/react-icons"
 
 export function Cart() {
   const orders = useOrderStore((state) => state.orders)
+  const increaseOrder = useOrderStore((state) => state.increaseOrder)
+  const decreaseOrder = useOrderStore((state) => state.decreaseOrder)
 
   return (
-    <div>
+    <div className='flex flex-col gap-2'>
       {orders.map((order, i) => (
         <Card key={i}>
           <CardHeader>
-            <CardTitle>
-              {order.product.name} x{order.quantity}
+            <CardTitle className='flex justify-between items-center'>
+              <span>{order.product.name}</span>
+              <span>P {order.product.price * order.quantity}</span>
             </CardTitle>
           </CardHeader>
-          <CardFooter>P {order.product.price * order.quantity}</CardFooter>
+          <CardFooter>
+            <div className='flex'>
+              <Button
+                className='rounded-none shadow-none first:rounded-s-lg last:rounded-e-lg focus-visible:z-10'
+                variant='outline'
+                size='icon'
+                onClick={() => increaseOrder(order)}
+              >
+                <ChevronUpIcon />
+              </Button>
+              <span className='flex items-center border border-input px-3 text-sm font-medium'>
+                {order.quantity}
+              </span>
+              <Button
+                className='rounded-none shadow-none first:rounded-s-lg last:rounded-e-lg focus-visible:z-10'
+                variant='outline'
+                size='icon'
+                onClick={() => decreaseOrder(order)}
+              >
+                <ChevronDownIcon />
+              </Button>
+            </div>
+          </CardFooter>
         </Card>
       ))}
     </div>
--- src/stores/use-order.store.ts
+++ src/stores/use-order.store.ts
@@ -4,6 +4,8 @@ import { create } from "zustand"
 interface OrderStore {
   orders: Order[]
   addOrder: (order: Order) => void
+  increaseOrder: (order: Order) => void
+  decreaseOrder: (order: Order) => void
   clearOrder: () => void
 }
 
@@ -24,6 +26,40 @@ export const useOrderStore = create<OrderStore>()((set) => ({
       return {
         orders: [...state.orders, order],
       }
+    }),
+  increaseOrder: (order: Order) =>
+    set((state) => {
+      const orderIndex = state.orders.findIndex(
+        (existingOrder) => existingOrder.product.id === order.product.id
+      )
+
+      if (orderIndex !== -1) {
+        const updatedOrders = [...state.orders]
+        updatedOrders[orderIndex].quantity += 1
+
+        return { orders: updatedOrders }
+      }
+
+      return state
+    }),
+  decreaseOrder: (order: Order) =>
+    set((state) => {
+      const orderIndex = state.orders.findIndex(
+        (existingOrder) => existingOrder.product.id === order.product.id
+      )
+
+      if (orderIndex !== -1) {
+        const updatedOrders = [...state.orders]
+        updatedOrders[orderIndex].quantity -= 1
+
+        if (updatedOrders[orderIndex].quantity === 0) {
+          updatedOrders.splice(orderIndex, 1)
+        }
+
+        return { orders: updatedOrders }
+      }
+
+      return state
     }),
   clearOrder: () =>
     set(() => ({
