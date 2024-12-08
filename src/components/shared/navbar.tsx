9036d46254e66d69e77729416c321bedeebb9723
jatnerubia <jasonlerit077@gmail.com>
2024-12-08 09:36:39 +0800

    update: navbar

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -1,23 +1,11 @@
 import { Cart } from "@/components/shared/cart"
-import { ModeToggle } from "@/components/shared/mode-toggle"
-import { Button } from "@/components/ui/button"
 import { SidebarTrigger } from "@/components/ui/sidebar"
-import { GitHubLogoIcon } from "@radix-ui/react-icons"
-import Link from "next/link"
 
 export const Navbar = () => {
   return (
     <header>
       <nav className='h-16 flex justify-between items-center'>
         <SidebarTrigger />
-        <div className='flex items-center gap-1'>
-          <Button asChild variant='ghost' size='icon' aria-label='github link'>
-            <Link href='#'>
-              <GitHubLogoIcon className='w-5 h-5' />
-            </Link>
-          </Button>
-          <ModeToggle />
-        </div>
         <Cart />
       </nav>
     </header>
