d6ac3035fd75afcbe664a45db4516c38e8cb2c72
jatnerubia <jasonlerit077@gmail.com>
2024-12-12 18:36:54 +0800

    feat: list products api

--- src/app/api/products/route.test.ts
+++ src/app/api/products/route.test.ts
@@ -1,52 +1,69 @@
-import { POST } from "@/app/api/products/route"
+import { GET, POST } from "@/app/api/products/route"
 import { describe, expect, it } from "vitest"
 
 describe("Products API", () => {
-  it("should return 201 for successful creation of product", async () => {
-    const requestObject = {
-      json: async () => ({ name: "Product 1", price: 100 }),
-    } as never
-    const response = await POST(requestObject)
-    expect(response.status).toBe(201)
+  describe("List", () => {
+    it("should return 200 and a list of products", async () => {
+      const requestObject = {
+        nextUrl: {
+          searchParams: new URLSearchParams({ page: "1" }),
+        },
+      } as never
+      const response = await GET(requestObject)
+      const body = await response.json()
+      expect(response.status).toBe(200)
+      expect(body).toHaveProperty("items")
+      expect(body).toHaveProperty("metadata")
+    })
   })
 
-  it("should return 400 for missing name field", async () => {
-    const requestObject = {
-      json: async () => ({ price: 100 }),
-    } as never
-    const response = await POST(requestObject)
-    expect(response.status).toBe(400)
-  })
+  describe("Create", () => {
+    it("should return 201 for successful creation of product", async () => {
+      const requestObject = {
+        json: async () => ({ name: "Product 1", price: 100 }),
+      } as never
+      const response = await POST(requestObject)
+      expect(response.status).toBe(201)
+    })
 
-  it("should return 400 for missing price field", async () => {
-    const requestObject = {
-      json: async () => ({ name: "Product 1" }),
-    } as never
-    const response = await POST(requestObject)
-    expect(response.status).toBe(400)
-  })
+    it("should return 400 for missing name field", async () => {
+      const requestObject = {
+        json: async () => ({ price: 100 }),
+      } as never
+      const response = await POST(requestObject)
+      expect(response.status).toBe(400)
+    })
 
-  it("should return 400 for invalid price field", async () => {
-    const requestObject = {
-      json: async () => ({ name: "Product 1", price: "invalid price" }),
-    } as never
-    const response = await POST(requestObject)
-    expect(response.status).toBe(400)
-  })
+    it("should return 400 for missing price field", async () => {
+      const requestObject = {
+        json: async () => ({ name: "Product 1" }),
+      } as never
+      const response = await POST(requestObject)
+      expect(response.status).toBe(400)
+    })
 
-  it("should return 400 for price lower than the minimum allowed", async () => {
-    const requestObject = {
-      json: async () => ({ name: "Product 1", price: -1 }),
-    } as never
-    const response = await POST(requestObject)
-    expect(response.status).toBe(400)
-  })
+    it("should return 400 for invalid price field", async () => {
+      const requestObject = {
+        json: async () => ({ name: "Product 1", price: "invalid price" }),
+      } as never
+      const response = await POST(requestObject)
+      expect(response.status).toBe(400)
+    })
 
-  it("should return 400 for price exceeding the maximum allowed", async () => {
-    const requestObject = {
-      json: async () => ({ name: "Product 1", price: 10_000_000 }),
-    } as never
-    const response = await POST(requestObject)
-    expect(response.status).toBe(400)
+    it("should return 400 for price lower than the minimum allowed", async () => {
+      const requestObject = {
+        json: async () => ({ name: "Product 1", price: -1 }),
+      } as never
+      const response = await POST(requestObject)
+      expect(response.status).toBe(400)
+    })
+
+    it("should return 400 for price exceeding the maximum allowed", async () => {
+      const requestObject = {
+        json: async () => ({ name: "Product 1", price: 10_000_000 }),
+      } as never
+      const response = await POST(requestObject)
+      expect(response.status).toBe(400)
+    })
   })
 })
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,6 +1,53 @@
 import db from "@/db"
 import { insertProductsSchema, products } from "@/db/schemas/products.schema"
+import { NextRequest } from "next/server"
 import { ZodError } from "zod"
+
+const PAGE_SIZE = 20
+
+export async function GET(req: NextRequest) {
+  try {
+    const searchParams = req.nextUrl.searchParams
+    const parsedPage = Number(searchParams.get("page") ?? 1)
+    const page = isNaN(parsedPage) ? 1 : parsedPage
+
+    const items = await db.query.products.findMany({
+      limit: PAGE_SIZE,
+      offset: (page - 1) * PAGE_SIZE,
+    })
+
+    const totalCount = await db.$count(products)
+    const totalPages = Math.ceil(totalCount / PAGE_SIZE)
+
+    const previousPage = page > 1 ? page - 1 : null
+    const nextPage = page < totalPages ? page + 1 : null
+
+    return Response.json({
+      items,
+      metadata: {
+        current_page: page,
+        previous_page: previousPage,
+        next_page: nextPage,
+        total_count: totalCount,
+        total_pages: totalPages,
+      },
+    })
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return Response.json(error, {
+        status: 400,
+      })
+    }
+    return Response.json(
+      {
+        message: "Internal server error",
+      },
+      {
+        status: 500,
+      }
+    )
+  }
+}
 
 export async function POST(req: Request) {
   try {
