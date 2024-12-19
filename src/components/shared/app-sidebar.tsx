983fbbe51248e79328e4a5da6fe38e2cfddad856
jatnerubia <jasonlerit077@gmail.com>
2024-12-19 17:44:45 +0800

    fix: sidebar menu button

--- src/components/shared/app-sidebar.tsx
+++ src/components/shared/app-sidebar.tsx
@@ -49,10 +49,10 @@ export function AppSidebar() {
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <a href={item.url}>
+                    <Link href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
-                    </a>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
